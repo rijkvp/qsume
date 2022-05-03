7826925034e2e109e9d51d8bd10056e52e2d1c7d
rijkvp <github@rijkvp.nl>
2022-05-03 17:25:44 +0200

    Improve packing

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,17 @@
 const path = require('path');
+const fs = require('fs');
+
+async function deleteSourceMaps(dir) {
+    const files = await fs.promises.readdir(dir);
+    for (const file of files) {
+        if (file.endsWith("js.map")) {
+            console.log(`Removing source map ${file}`);
+            await fs.promises.unlink(dir + "/" + file);
+        }
+    }
+}
+
+deleteSourceMaps("./dist");
 
 module.exports = {
     entry: './src/main.ts',
@@ -18,5 +31,4 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    watch: true,
 };
